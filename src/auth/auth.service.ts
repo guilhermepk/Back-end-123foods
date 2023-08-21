f17e7f99b9afbd030a475b2325f55e833dbed93b
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2023-08-21 11:53:59 -0300

    adicionando item no payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,50 +1,45 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
-import {JwtService} from "@nestjs/jwt";
-import {Users} from "../users/entities/users.entity";
-import {TokenService} from "../token/token.service";
-
+import { JwtService } from '@nestjs/jwt';
+import { Users } from '../users/entities/users.entity';
+import { TokenService } from '../token/token.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService:UsersService,
-                private jwtService: JwtService,
-                private tokenService: TokenService){}
-    async validateUser(email: string, password: string): Promise<any> {
-        const user = await this.usersService.findOne(email);
-        //console.log(user);
-        if (user && bcrypt.compareSync(password, user.password)) {
-
-            const {password, ...result} = user;
-            // TODO: Generate a JWT and return it here
-            // instead of the user object
-            return result;
-        }
-        return null;
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+    private tokenService: TokenService,
+  ) {}
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(email);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const { password, ...result } = user;
+      return result;
     }
+    return null;
+  }
 
-    async login(user:any) {
-
-        const payload = {username: user.name, sub: user.id};
-        console.log('payload:', payload)
-        const token = this.jwtService.sign(payload)
-        this.tokenService.save(token, user.email)
-        return {
-            access_token: this.jwtService.sign(payload),
-
-        };
+  async login(user: any) {
+    const payload = { username: user.name, sub: user.id, admin: user.admin };
+    const token = this.jwtService.sign(payload);
+    this.tokenService.save(token, user.email);
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+  async loginToken(token: string) {
+    const usuario: Users = await this.tokenService.getUsuarioByToken(token);
+    if (usuario) {
+      return this.login(usuario);
+    } else {
+      return new HttpException(
+        {
+          errorMessage: 'Token inválido',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
     }
-    async loginToken(token: string) {
-        let usuario: Users = await this.tokenService.getUsuarioByToken(token)
-        if (usuario){
-            return this.login(usuario)
-        }else{
-            return new HttpException({
-                errorMessage: 'Token inválido'
-            }, HttpStatus.UNAUTHORIZED)
-        }
-    }
-
+  }
 }
-
