3fa20a6b40e6b4532e24b50faa8e5cd50d736a66
Vinicius Gabriel Graupmann Juraszek <102488557+viniggj2005@users.noreply.github.com>
2023-08-25 12:16:43 -0300

    protegendo o address

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,37 +1,53 @@
 import {
   Controller,
+  Request,
   Get,
   Post,
   Body,
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UploadedFile,
+  
+  BadRequestException,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from '../auth/auth.service';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
 
 @Controller('address')
 export class AddressController {
-  constructor(private readonly addressService: AddressService) {}
+  constructor(private readonly addressService: AddressService,
+    private authService: AuthService,) {}
 
+    @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createAddressDto: CreateAddressDto) {
     return this.addressService.create(createAddressDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get(':userId')
   findAll(@Param('userId') userId: string) {
     return this.addressService.findAll(+userId);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAddressDto: UpdateAddressDto) {
     return this.addressService.update(+id, updateAddressDto);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.addressService.remove(+id);
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/address/address.module.ts
+++ src/address/address.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import {forwardRef, Module} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { AddressController } from './address.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Address } from './entities/address.entity';
+import {AuthService} from "../auth/auth.service";
+import {AuthModule} from "../auth/auth.module";
 @Module({
-  imports: [TypeOrmModule.forFeature([Address])],
+  imports: [TypeOrmModule.forFeature([Address]),
+  forwardRef(()=>AuthModule)],
   controllers: [AddressController],
   providers: [AddressService],
   exports: [AddressService],
