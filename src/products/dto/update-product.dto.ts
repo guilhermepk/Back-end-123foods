0b506e6d4b748d1755488c5a740c60861fb4690c
viniggj2005 <102488557+viniggj2005@users.noreply.github.com>
2023-09-12 18:14:22 -0300

    rota relativa para evitar erro

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -2,5 +2,5 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {
-  unit_of_measurement: import("/home/administrador/Documentos/GitHub/nestjs-typeorm-alura-lesson-2/src/units_of_meansurement/entities/units_of_meansurement.entity").UnitsOfMeansurement[];
+  unit_of_measurement: import('../../units_of_meansurement/entities/units_of_meansurement.entity').UnitsOfMeansurement[];
 }
